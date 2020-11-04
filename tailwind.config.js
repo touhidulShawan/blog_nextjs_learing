89260820a1f534b7efe234d20b18947fddbe891d
Touhidul Shawan <touhidulshawan@gmail.com>
2020-11-05 02:44:55 +0600

    purse css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx", "./styles/**/*.css"],
   theme: {
     extend: {},
   },
